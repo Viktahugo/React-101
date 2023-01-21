66ece9d6d0e2769a2db7a0425bbdbd09d70a9897
VictorHugo Bisangwa <Viktahugo91@gmail.com>
2023-01-22 01:28:11 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react' //This Imports the useState Hook from react
+                              // Hook begins with keyword use
 
-function App() {
+// New React Componet
+const Person = (props) => {
+   return(
+    <>
+    <h1> Name: {props.name}</h1>
+    <h2>Last Name: {props.lastName}</h2>
+    <h3>Age: {props.age}</h3>
+    </>
+   );
+}
+
+const ShowButton =() =>{
+  const [Counter,setCounter] = useState(0); 
+   useEffect(()=> {
+       setCounter(100)
+  },[]);   
+
+  return(
+    <>
+    <button onClick={()=>setCounter((prevCount)=>prevCount -1)}>-</button>
+    <h1>{Counter}</h1>
+    <button onClick ={()=>setCounter((prevCount)=>prevCount + 1)}>+</button>     
+    </>
+  );
+}
+
+// Default Page Componet
+const App = () => {
+  const name = "NewBie"
+  const isNameShowing=true 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <h1> Hello, {isNameShowing ? name : "Pro"}  </h1>
+
+          <h2>Welcome to REACT</h2>
+        <Person name={'Leandro'} lastName={'Trossard'} age={'26 Years'}/> {/*}This calls the React Componet named Person  {*/}
+        <Person name ={'John'} lastName={'Doe'} age={'30 years'}/>
+        <ShowButton />
     </div>
   );
 }
